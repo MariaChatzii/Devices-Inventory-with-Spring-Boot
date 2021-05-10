10c575c6afa2c8d4d4f8e9bdcd31d7d0cad00ffd
MariaChatzii <myprogrammingcareer1>
2021-05-10 11:40:24 +0300

    using only one mapper to dto class

--- src/main/java/com/inventory/deviceInventory/mapper/DTOMapper.java
+++ src/main/java/com/inventory/deviceInventory/mapper/DTOMapper.java
@@ -1,14 +1,22 @@
 package com.inventory.deviceInventory.mapper;
 
 import com.inventory.deviceInventory.DTO.DeviceDTO;
+import com.inventory.deviceInventory.DTO.EmployeeDTO;
 import com.inventory.deviceInventory.entity.Device;
+import com.inventory.deviceInventory.entity.Employee;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface DeviceDeviceDTOMapper {
+public interface DTOMapper {
+    @Mapping(source = "employee.company.name", target = "companyName")
+    @Mapping(source = "employee.company.address", target = "companyAddress")
+    @Mapping(expression = "java(employee.getDevices().size())", target = "devicesCount")
+    EmployeeDTO employeeToEmployeeDTO(Employee employee);
+    List<EmployeeDTO> employeesToEmployeeDTOs(List<Employee> employees);
+
     @Mapping(source = "device.companyOwner.name", target = "companyOwnerName")
     @Mapping(source = "device.companyOwner.address", target = "companyOwnerAddress")
     @Mapping(source = "device.employeeOwner.name", target = "employeeOwnerName")
