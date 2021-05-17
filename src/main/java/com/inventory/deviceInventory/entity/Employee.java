47519568544e15ec094e58c091457f177a22bd20
MariaChatzii <myprogrammingcareer1>
2021-05-17 21:50:19 +0300

    make 'name' and 'email' column in Employee Entity not to be blank or empty

--- src/main/java/com/inventory/deviceInventory/entity/Employee.java
+++ src/main/java/com/inventory/deviceInventory/entity/Employee.java
@@ -5,6 +5,8 @@ import lombok.*;
 import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Getter
@@ -21,15 +23,16 @@ public class Employee {
     private Integer id;
 
     @Column(name = "name", nullable = false, length = 45)
-    @NonNull
+    @NotEmpty(message = "Name may not be empty")
+    @NotBlank(message = "Name may not be blank")
     private String name;
 
     @Column(name = "email", unique = true, nullable = false, length = 45)
-    @NonNull
+    @NotEmpty(message = "Email may not be empty")
+    @NotBlank(message = "Email may not be blank")
     private String email;
 
-    //@JsonManagedReference
-    @OneToMany(mappedBy = "employeeOwner", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "employeeOwner")
     private List<Device> devices;
 
     @ManyToOne
